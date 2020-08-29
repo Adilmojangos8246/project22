331249dfedfda26acbbfcbbd3c6dfca6b0a792a0
Adilmojangos8246 <67742933+Adilmojangos8246@users.noreply.github.com>
2020-08-29 20:11:50 +0530

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -13,16 +13,20 @@ function preload()
 
 function setup() {
 	createCanvas(800, 700);
-	rectMode(CENTER);
-	
+	//rectMode(CENTER);
 
 	packageSprite=createSprite(width/2, 80, 10,10);
 	packageSprite.addImage(packageIMG)
 	packageSprite.scale=0.2
 
-	helicopterSprite=createSprite(width/2, 200, 10,10);
+	helicopterSprite=createSprite(width/2, 80, 10,10);
 	helicopterSprite.addImage(helicopterIMG)
 	helicopterSprite.scale=0.6
+	//helicopterSprite.depth = packageSprite.depth + 1;
+
+	
+    
+	
 
 	groundSprite=createSprite(width/2, height-35, width,10);
 	groundSprite.shapeColor=color(255)
@@ -31,7 +35,7 @@ function setup() {
 	engine = Engine.create();
 	world = engine.world;
 
-	packageBody = Bodies.circle(width/2 , 200 , 5 , {restitution:3, isStatic:true});
+	packageBody = Bodies.circle(width/2 , 200 , 5 , {restitution:0.6, isStatic:false});
 	World.add(world, packageBody);
 	
 
@@ -48,16 +52,15 @@ function setup() {
 function draw() {
   rectMode(CENTER);
   background(0);
-  packageSprite.x= packageBody.position.x 
-  packageSprite.y= packageBody.position.y 
+ 
   drawSprites();
  
 }
 
 function keyPressed() {
  if (keyCode === DOWN_ARROW) {
-    // Look at the hints in the document and understand how to make the package body fall only on
-    
+    packageSprite.x= packageBody.position.x; 
+  	packageSprite.y= packageBody.position.y ;    
   }
 }
 
